a04bf97d721751625a6c325d972a3f67985fe8c1
MONTASSER AL AMSAKI <alamsakimontasser@gmail.com>
2022-05-17 11:44:35 +0100

    ok

--- app/src/main/java/com/example/mytestcontrole1/MainActivity.java
+++ app/src/main/java/com/example/mytestcontrole1/MainActivity.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+     TextView tc;
+     EditText tt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tc=findViewById(R.id.tt);
+        tt=findViewById(R.id.et);
+
     }
 }
